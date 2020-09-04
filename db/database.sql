3bb289f11c5805031b20a731127a81e32ef8d157
alifaraji <alifaraji65452@gmail.com>
2020-09-04 20:09:10 +0430

    first commit"

---
+++ db/database.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `user`(
+    `user_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,AUTO_INCREMENT=1,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL
+)
+ALTER TABLE `user`
+MODIFY `user_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,AUTO_INCREMENT=1;
+
+CREATE TABLE `position`(
+    `position_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `position_name` varchar(255) NOT NULL,
+    `time` datetime DEFAULT NOW()
+);
+INSERT INTO `position`(`position_name`,`time`) VALUES
+('doctor'),
+('web developer'),
+('designer'),
+('marketing manager');
+
+ALTER TABLE `position` MODIFY `position_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+CREATE TABLE `candidate`(
+    `candidate_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `candidate_name` varchar(255) NOT NULL,
+    `candidate_position` varchar(255) NOT NULL,
+    `time` datetime DEFAULT NOW()
+);
+INSERT INTO `candidate`(`candidate_name`,`candidate_position`) VALUES
+('john doe','designer'),
+('aref faraji','prime-minister'),
+('yadollah faraji','doctor'),
+('maryam parvaneh','driver');
+
+ALTER TABLE `user` ADD `is_admin` varchar(10) NOT NULL DEFAULT 'no';
+
---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+require('./db/DBController.php');
+require('./db/position.php');
+require('./db/candidate.php');
+$db = new DBController();
+$position = new Positon($db);
+$candidate = new Candidate($db);
+$restrict_page = function () {
+    //user is not logged-in
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    //if user is not logged in we are going to redirect it to the login page
+    if ($_SESSION['logged-in'] == 'no') {
+        header('Location:login.php?session');
+        exit();
+    }
+};
